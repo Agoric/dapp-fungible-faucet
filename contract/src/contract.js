38aeee793b3a59d07eeacc254de0316668338560
Kate Sills <kate@agoric.com>
2021-11-15 15:50:22 -0800

    chore: harden amountKeywordRecord

--- contract/src/contract.js
+++ contract/src/contract.js
@@ -32,7 +32,7 @@ const start = async (zcf) => {
   const mintPayment = (seat) => {
     const amount = AmountMath.make(brand, 1000n);
     // Synchronously mint and allocate amount to seat.
-    zcfMint.mintGains({ Token: amount }, seat);
+    zcfMint.mintGains(harden({ Token: amount }), seat);
     // Exit the seat so that the user gets a payout.
     seat.exit();
     // Since the user is getting the payout through Zoe, we can
