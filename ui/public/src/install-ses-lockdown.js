66309e2ccf4dd7cf296b89738864936781ac6ce8
samsiegart <samsiegart@gmail.com>
2022-08-28 11:48:18 -0700

    chore: use unsafe only for dev

--- ui/public/src/install-ses-lockdown.js
+++ ui/public/src/install-ses-lockdown.js
@@ -3,11 +3,13 @@
 import 'ses/dist/ses.umd'; // adds lockdown, harden, and Compartment
 import '@endo/eventual-send/shim'; // adds support needed by E
 
+const errorTaming = process.env.NODE_ENV === 'development' ? 'unsafe' : 'safe';
+
 // Help lock down the JS environment.  The start compartment (current evaluation context)
 // can still access powerful globals, but this start compartment can use `new Compartment(...)`
 // to evaluate code with stricter confinement.
 lockdown({
-  errorTaming: 'unsafe', // TODO Wen safe?
+  errorTaming,
   overrideTaming: 'severe',
 });
 
